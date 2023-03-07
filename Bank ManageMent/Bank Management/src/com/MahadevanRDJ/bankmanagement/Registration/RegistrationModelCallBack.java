033c0f3bf9ed6e8619d6a5acc9fc887cabcd9073
Mahadevan <118713713+MahadevanRDJ@users.noreply.github.com>
2023-03-07 18:42:07 +0530

    Delete Bank ManageMent directory

--- Bank ManageMent/Bank Management/src/com/MahadevanRDJ/bankmanagement/Registration/RegistrationModelCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.MahadevanRDJ.bankmanagement.Registration;
-
-public interface RegistrationModelCallBack {
-
-    void addCustomer(String firstName, String lastName, int age, String gender, String city, String district, long contact);
-
-    void displayCustomer(int customerID);
-    
-}
