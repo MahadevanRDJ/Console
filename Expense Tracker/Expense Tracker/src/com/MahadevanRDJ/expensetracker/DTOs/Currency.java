50c5d807c3a661162c493beb3ae87997711dd1f4
Mahadevan <118713713+MahadevanRDJ@users.noreply.github.com>
2023-03-07 18:33:56 +0530

    Delete Expense Tracker directory

--- Expense Tracker/Expense Tracker/src/com/MahadevanRDJ/expensetracker/DTOs/Currency.java
+++
@@ -1,5 +0,0 @@
-package com.MahadevanRDJ.expensetracker.DTOs;
-
-public enum Currency {
-    INR, USD, EUR, GBP;
-}
--- Expense Tracker/Expense Tracker/src/com/MahadevanRDJ/expensetracker/Wallet/WalletViewCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.MahadevanRDJ.expensetracker.Wallet;
-
-import com.MahadevanRDJ.expensetracker.DTOs.Wallet;
-
-public interface WalletViewCallBack {
-
-    void displayWallet(Wallet wallet);
-    
-}
